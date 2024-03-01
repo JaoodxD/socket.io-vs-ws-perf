ce10e4d5bbbd32332feb27ae55bbc79c444a34c9
JaoodxD <jaood96@gmail.com>
2024-03-01 15:03:05 +0200

    Add tracking of number of finished

--- io-client.js
+++ io-client.js
@@ -3,6 +3,7 @@ const { io } = require('socket.io-client')
 const SERVER_URL = process.argv[2] || 'localhost'
 const concurrency = process.argv[3] || 1
 
+let closed = 0
 for (let i = 1; i <= concurrency; i++) {
   console.time('connect ' + i)
   const socket = io(`ws://${SERVER_URL}:3000`, {
@@ -13,6 +14,9 @@ for (let i = 1; i <= concurrency; i++) {
   socket.on('custom_ping', () => {
     socket.emit('custom_pong')
   })
-  socket.io.on('close', () => console.timeEnd('connect ' + i))
+  socket.io.on('close', () => {
+    console.timeEnd('connect ' + i)
+    console.log('closed:', ++closed)
+  })
   socket.io.on('error', console.error)
 }
