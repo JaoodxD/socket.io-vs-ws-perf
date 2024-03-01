c515c5d0167eb81dd86fffe3ea7161804fbc32f7
JaoodxD <jaood96@gmail.com>
2024-03-01 13:10:01 +0200

    Add option to pass concurrency to cmd args

--- io-client.js
+++ io-client.js
@@ -1,8 +1,16 @@
 const { io } = require('socket.io-client')
 
 const SERVER_URL = process.argv[2] || 'localhost'
-const socket = io(`ws://${SERVER_URL}:3000`)
+const concurrency = process.argv[3] || 1
 
-socket.on('custom_ping', () => {
-  socket.emit('custom_pong')
-})
+for (let i = 1; i <= concurrency; i++) {
+  console.time('connect ' + i)
+  const socket = io(`ws://${SERVER_URL}:3000`, {
+    forceNew: true
+  })
+
+  socket.on('custom_ping', () => {
+    socket.emit('custom_pong')
+  })
+  socket.io.on('close', () => console.timeEnd('connect ' + i))
+}
--- ws-client.js
+++ ws-client.js
@@ -1,8 +1,11 @@
 const { WebSocket } = require('ws')
 
 const SERVER_URL = process.argv[2] || 'localhost'
+const concurrency = process.argv[3] || 1
 
-console.time('connect')
-const ws = new WebSocket(`ws://${SERVER_URL}:8080`)
+for (let i = 1; i <= concurrency; i++) {
+  console.time('connect ' + i)
+  const ws = new WebSocket(`ws://${SERVER_URL}:8080`)
 
-ws.on('close', () => console.timeEnd('connect'))
+  ws.on('close', () => console.timeEnd('connect ' + i))
+}
