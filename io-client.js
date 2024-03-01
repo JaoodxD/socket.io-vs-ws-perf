d4caa305417ea5e0b72042973531465f2d8d60d8
JaoodxD <jaood96@gmail.com>
2024-03-01 12:45:07 +0200

    Add option to pass server url as cmd arg

--- io-client.js
+++ io-client.js
@@ -1,5 +1,7 @@
 const { io } = require('socket.io-client')
-const socket = io('ws://localhost:3000')
+
+const SERVER_URL = process.argv[2] || 'localhost'
+const socket = io(`ws://${SERVER_URL}:3000`)
 
 socket.on('custom_ping', () => {
   socket.emit('custom_pong')
--- ws-client.js
+++ ws-client.js
@@ -1,6 +1,8 @@
 const { WebSocket } = require('ws')
 
+const SERVER_URL = process.argv[2] || 'localhost'
+
 console.time('connect')
-const ws = new WebSocket('ws://localhost:8080')
+const ws = new WebSocket(`ws://${SERVER_URL}:8080`)
 
 ws.on('close', () => console.timeEnd('connect'))
