f8eae6c8c0f5cf3d2ca381ebc550d57aa45c5900
JaoodxD <jaood96@gmail.com>
2024-03-01 15:04:54 +0200

    Add logging closed connections for ws client

--- ws-client.js
+++ ws-client.js
@@ -3,10 +3,15 @@ const { WebSocket } = require('ws')
 const SERVER_URL = process.argv[2] || 'localhost'
 const concurrency = process.argv[3] || 1
 
+let closed = 0
 for (let i = 1; i <= concurrency; i++) {
   console.time('connect ' + i)
   const ws = new WebSocket(`ws://${SERVER_URL}:8080`)
 
-  ws.on('close', () => console.timeEnd('connect ' + i))
+  ws.on('close', () => {
+    console.timeEnd('connect ' + i)
+    console.log('closed:', ++closed)
+
+  })
   ws.on('error', console.error)
 }
