0ccf003dd0a6ee4f57dd21b2055bd15df40cc1fe
JaoodxD <jaood96@gmail.com>
2024-03-04 16:25:07 +0200

    chore prettify

--- io-server.js
+++ io-server.js
@@ -1,33 +1,16 @@
 const { Server } = require('socket.io')
-const hrtime = process.hrtime.bigint
-const diff = (ts1, ts2) => (Number(ts2 - ts1) / 1e6) | 0
 
 const io = new Server()
 
 io.on('connection', socket => {
   console.log('a user connected')
   let pingCount = 0
-  // let pings = []
-  // let pongs = []
-  // let diffs = []
 
   socket.on('custom_pong', () => {
-    // const time = hrtime()
-    // pongs.push(time)
-    // diffs.push(diff(pings[pings.length - 1], time))
     pingCount++
-    if (pingCount < 1_000) {
-      // pings.push(hrtime())
-      socket.emit('custom_ping')
-    } else {
-      socket.disconnect()
-      // console.table(pings.map((ms, n) => ({ ping: ms, pong: pongs[n], diff: diffs[n] })))
-      // const sum = diffs.reduce((acc, n) => acc + n, 0)
-      // const avg = sum / diffs.length
-      // console.log(sum, avg)
-    }
+    if (pingCount < 100) socket.emit('custom_ping')
+    else socket.disconnect()
   })
-  // pings.push(hrtime())
   socket.emit('custom_ping')
 })
 
--- ws-server.js
+++ ws-server.js
@@ -1,6 +1,4 @@
 const { WebSocketServer } = require('ws')
-const hrtime = process.hrtime.bigint
-const diff = (ts1, ts2) => (Number(ts2 - ts1) / 1e6) | 0
 
 const server = new WebSocketServer({
   port: 8080,
@@ -10,28 +8,11 @@ const server = new WebSocketServer({
 server.on('connection', (conn, req) => {
   console.log('connected')
   let pingCount = 0
-  // let pings = []
-  // let pongs = []
-  // let diffs = []
 
   conn.on('pong', () => {
-    // const time = hrtime()
-    // pongs.push(time)
-    // diffs.push(diff(pings[pings.length - 1], time))
     pingCount++
-    if (pingCount < 1_000) {
-      conn.ping()
-      // pings.push(hrtime())
-    } else {
-      conn.close()
-      // console.table(
-      //   pings.map((ms, n) => ({ ping: ms, pong: pongs[n], diff: diffs[n] }))
-      // )
-      // const sum = diffs.reduce((acc, n) => acc + n, 0)
-      // const avg = sum / diffs.length
-      // console.log(sum, avg)
-    }
+    if (pingCount < 100) conn.ping()
+    else conn.close()
   })
-  // pings.push(hrtime())
   conn.ping()
 })
